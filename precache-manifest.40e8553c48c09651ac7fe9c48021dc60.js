2ed3f6f8215a3730387c2e1abe602f556db6aa66
amcolash <amcolash@gmail.com>
2020-01-05 04:48:29 +0000

    Deploying to gh-pages from  c4b93913b8ece80029138e996d96f4071de4b048

---
+++ precache-manifest.40e8553c48c09651ac7fe9c48021dc60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06590a7999f7a67188d62b7b2cd91c86",
+    "url": "/led_spectrum_remote/index.html"
+  },
+  {
+    "revision": "482b6773216e053132e8",
+    "url": "/led_spectrum_remote/static/css/main.d9f5b5e8.chunk.css"
+  },
+  {
+    "revision": "c0470826cd3ea9a18266",
+    "url": "/led_spectrum_remote/static/js/2.1d17dda0.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/led_spectrum_remote/static/js/2.1d17dda0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "482b6773216e053132e8",
+    "url": "/led_spectrum_remote/static/js/main.49e1d7bf.chunk.js"
+  },
+  {
+    "revision": "0c092fa7b7a4e85856f3",
+    "url": "/led_spectrum_remote/static/js/runtime-main.ece4b47a.js"
+  }
+]);
